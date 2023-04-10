0f672d101e4261f9fb12e80897386266b441238a
Zuka Jeladze <zukajeldze14@gmail.com>
2023-04-11 03:25:49 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Input, Space } from 'antd';
+import './index.css';
+import axios from 'axios';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState('');
+  const { TextArea } = Input;
+  const fetchData = async () => {
+    try {
+      await axios
+        .post('https://uniswap-contract-checker.herokuapp.com/', {
+          filterName: 'Artemis',
+        })
+        .then((respose) => setData(respose));
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2 style={{ marginBottom: 0 }}>Uniswap Graph Checker</h2>
+      <Space>
+        <Input
+          style={{ width: '20em' }}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Token name or symbol"
+        ></Input>
+        <Button
+          type="primary"
+          onClick={() => {
+            fetchData();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Check
+        </Button>
+      </Space>
+      <TextArea
+        style={{ width: '80%' }}
+        placeholder="Results will be shown here"
+        rows={40}
+        value={data.toString()}
+      ></TextArea>
     </div>
   );
 }
